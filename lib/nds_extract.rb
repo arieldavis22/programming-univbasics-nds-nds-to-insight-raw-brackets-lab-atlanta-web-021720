99963fee16728c703f3957fd5529ee41ad4b35ce
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-18 02:40:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,5 +24,5 @@ def directors_totals(nds)
     result[directors_database[name_index]] = directors_database[name_index][:movies][0][:worldwide_gross]
     name_index += 1
   end
-  result
+  p result
 end
